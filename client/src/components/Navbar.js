f73348c289535fec1d4a415a3e367cf018e585e9
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2023-08-07 16:10:45 -0300

    advanced styling the navbar for mobile devices

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,19 +1,60 @@
 import style from './Navbar.module.scss'
 
+import { useState } from 'react'
+
 function Navbar(){
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const navigableRoutes = [
+    {
+      name: 'Home'
+    },
+    {
+      name: 'Sobre'
+    },
+    {
+      name: 'Contato'
+    }
+  ]
+
   return (
-    <nav className={style['navbar']}>
+    <div className={style['navbar']}>
       <div className='container'>
-        <div id={style['brand']}>
+        <div
+          className={style['brand']}
+          expanded={isExpanded ? 'true' : 'false'}
+        >
           <img src="" alt="Institut Psyché" />
         </div>
-        <button className={style['toggle']}>
+        <button
+          className={style['toggle']}
+          onClick={() => setIsExpanded(!isExpanded)}
+          expanded={isExpanded ? 'true' : 'false'}
+        >
           <div className={style['line']} />
           <div className={style['line']} />
           <div className={style['line']} />
         </button>
       </div>
-    </nav>
+      <div
+        className={style['expanded-menu']}
+        expanded={isExpanded ? 'true' : 'false'}
+      >
+          <div className={style['expanded-menu-inner']}>
+            <ul className={style['navigation']}>
+              {
+                navigableRoutes.map(
+                  function (route) {
+                    return (
+                      <li className={style['nav-item']}>{route.name}</li>
+                    );
+                  }
+                )
+              }
+            </ul>
+          </div>
+      </div>
+    </div>
   );
 }
 
