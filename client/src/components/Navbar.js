ab6f4b8606b89f96c9fc610cd3ed5a1de8dcfed6
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2023-08-14 12:07:01 -0300

    made the navbar responsive for devices up to small laptops

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -31,42 +31,40 @@ function Navbar(props){
           <div className={style['line']} />
           <div className={style['line']} />
         </button>
-      </div>
-      <div
-        className={style['expanded-menu']}
-        expanded={isExpanded ? 'true' : 'false'}
-      >
-          <div className={style['expanded-menu-inner']}>
-            <ul className={style['navigation']}>
-              {
-                props.routes.map(
-                  function (route, key) {
-                    return (
-                      <li key={key} className={style['nav-item']}>
-                        <NavLink to={route.path} onClick={() => {setIsExpanded(false)}}>
-                          {route.name}
-                        </NavLink>
-                      </li>
-                    );
-                  }
-                )
-              }
-            </ul>
-            <div className={style['contact-info']}>
-              <a
-                className={`${style['contact-button']} ${style['altered']}`}
-                href='tel:00000000000'
-              >
-                <AiOutlinePhone className={style['icon']} />(00) 00000-0000
-              </a>
-              <a
-                className={`${style['contact-button']} ${style['regular']}`}
-                href='tel:00000000000'
-              >
-                <AiOutlineWhatsApp className={style['icon']} />(00) 00000-0000
-              </a>
-            </div>
+        <div
+          className={style['menu']}
+          expanded={isExpanded ? 'true' : 'false'}
+        >
+          <ul className={style['navigation']}>
+            {
+              props.routes.map(
+                function (route, key) {
+                  return (
+                    <li key={key} className={style['nav-item']}>
+                      <NavLink to={route.path} onClick={() => {setIsExpanded(false)}}>
+                        {route.name}
+                      </NavLink>
+                    </li>
+                  );
+                }
+              )
+            }
+          </ul>
+          <div className={style['contact-info']}>
+            <a
+              className={`${style['contact-button']} ${style['altered']}`}
+              href='tel:00000000000'
+            >
+              <AiOutlinePhone className={style['icon']} />(00) 00000-0000
+            </a>
+            <a
+              className={`${style['contact-button']} ${style['regular']}`}
+              href='tel:00000000000'
+            >
+              <AiOutlineWhatsApp className={style['icon']} />(00) 00000-0000
+            </a>
           </div>
+        </div>
       </div>
     </div>
   );
