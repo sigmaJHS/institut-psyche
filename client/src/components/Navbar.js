f328e9368df9d7a770bd70db3949d64d7f195734
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2023-08-22 17:44:10 -0300

    removed flex as default display of container

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -20,64 +20,66 @@ export default function Navbar(props){
   return (
     <div className={style['navbar']}>
       <div className='container'>
-        <div
-          className={style['brand']}
-          expanded={isExpanded ? 'true' : 'false'}
-        >
-          <NavLink to='/' onClick={() => {setIsExpanded(false)}}>
-            <img
-              src={logo}
-              alt="Institut Psyché" />
-          </NavLink>
-        </div>
-        <button
-          className={style['toggle']}
-          onClick={() => setIsExpanded(!isExpanded)}
-          expanded={isExpanded ? 'true' : 'false'}
-        >
-          <div className={style['line']} />
-          <div className={style['line']} />
-          <div className={style['line']} />
-        </button>
-        <div
-          className={style['menu']}
-          expanded={isExpanded ? 'true' : 'false'}
-        >
-          <ul className={style['navigation']}>
-            {
-              props.routes.map(
-                function (route, key) {
-                  return (
-                    <li key={key} className={style['nav-item']}>
-                      <NavLink to={route.path} onClick={() => {setIsExpanded(false)}}>
-                        {route.name}
-                      </NavLink>
-                    </li>
-                  );
-                }
-              )
-            }
-          </ul>
-          <div className={style['contact-info']}>
-            <div className={style['contact-text']}>(00) 00000-0000</div>
-            <div className={style['buttons']}>
-              <a
-                className={`${style['contact-button']} ${style['regular']}`}
-                href='tel:00000000000'
-              >
-                <FaPhoneAlt className={style['icon']} />
-              </a>
-              <a
-                className={`${style['contact-button']} ${style['altered']}`}
-                href='tel:00000000000'
-              >
-                <FaWhatsapp className={style['icon']} />
-              </a>
-              <div
-                className={`${style['contact-button']} ${style['altered']}`}
-                onClick={() => navigator.clipboard.writeText('(00) 00000-0000')}
-              >
-                <FaRegCopy className={style['icon']} />
+        <div className={style['navbar-inner']}>
+          <div
+            className={style['brand']}
+            expanded={isExpanded ? 'true' : 'false'}
+          >
+            <NavLink to='/' onClick={() => {setIsExpanded(false)}}>
+              <img
+                src={logo}
+                alt="Institut Psyché" />
+            </NavLink>
+          </div>
+          <button
+            className={style['toggle']}
+            onClick={() => setIsExpanded(!isExpanded)}
+            expanded={isExpanded ? 'true' : 'false'}
+          >
+            <div className={style['line']} />
+            <div className={style['line']} />
+            <div className={style['line']} />
+          </button>
+          <div
+            className={style['menu']}
+            expanded={isExpanded ? 'true' : 'false'}
+          >
+            <ul className={style['navigation']}>
+              {
+                props.routes.map(
+                  function (route, key) {
+                    return (
+                      <li key={key} className={style['nav-item']}>
+                        <NavLink to={route.path} onClick={() => {setIsExpanded(false)}}>
+                          {route.name}
+                        </NavLink>
+                      </li>
+                    );
+                  }
+                )
+              }
+            </ul>
+            <div className={style['contact-info']}>
+              <div className={style['contact-text']}>(11) 97165-8694</div>
+              <div className={style['buttons']}>
+                <a
+                  className={`${style['contact-button']} ${style['regular']}`}
+                  href='tel:11971658694'
+                >
+                  <FaPhoneAlt className={style['icon']} />
+                </a>
+                <a
+                  className={`${style['contact-button']} ${style['altered']}`}
+                  href='https://wa.me/5511971658694'
+                >
+                  <FaWhatsapp className={style['icon']} />
+                </a>
+                <div
+                  className={`${style['contact-button']} ${style['altered']}`}
+                  onClick={() => navigator.clipboard.writeText('(00) 00000-0000')}
+                >
+                  <FaRegCopy className={style['icon']} />
+                </div>
               </div>
             </div>
           </div>
