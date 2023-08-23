55b4ec2babdf3e72db3ca66188aa7b95734fdefa
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2023-08-23 13:54:15 -0300

    Replaced the "for" attribute in label tags by "htmlFor"

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -4,15 +4,15 @@ export default function ContactForm () {
   return (
     <form className={style['form']}>
       <div className={style['form-row']}>
-        <label for='name'>Seu nome:</label>
+        <label htmlFor='name'>Seu nome:</label>
         <input type='text' id='name' name='name' />
       </div>
       <div className={style['form-row']}>
-        <label for='email'>Seu email:</label>
+        <label htmlFor='email'>Seu email:</label>
         <input type='email' id='email' name='email' />
       </div>
       <div className={style['form-row']}>
-        <label for='message'>Sua Mensagem:</label>
+        <label htmlFor='message'>Sua Mensagem:</label>
         <textarea rows='8' id='message' name='message' />
       </div>
       <div className={style['form-row']}>
