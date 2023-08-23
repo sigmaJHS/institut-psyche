77f7e8882130800fafcb9ef40790a88e4b6f0425
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2023-08-23 17:49:11 -0300

    Made an upper section for the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,11 +3,34 @@ import { NavLink } from 'react-router-dom';
 import style from './Footer.module.scss'
 import logo from './../assets/institut-psyche.png'
 
+import links from './../const/links'
+
 import ContactLinks from './../components/ContactLinks';
 
 export default function Footer () {
+
+  const footerTopSocialLinks = ['whatsapp', 'instagram', 'facebook', 'youtube'];
+  const footerInnerSocialLinks = ['phone', 'email', 'address'];
+
   return (
     <div className={style['footer']}>
+        <div className={style['footer-top']}>
+          {
+            links.map(
+              function (link, key) {
+                return (footerTopSocialLinks.includes(link.name))
+                ? (
+                  <div key={key} className={style['social-button']}>
+                    <a href={link.href}>
+                      <link.icon className={style['icon']} />
+                    </a>
+                  </div>
+                )
+                : null
+              }
+            )
+          }
+        </div>
         <div className={style['footer-inner']}>
           <div class='container'>
             <div className={style['brand']}>
@@ -15,7 +38,10 @@ export default function Footer () {
                 <img src={logo} alt='Institut Psyché' />
               </NavLink>
             </div>
-            <ContactLinks className={style['links']} />
+            <ContactLinks
+              className={style['links']}
+              include={footerInnerSocialLinks}
+            />
           </div>
         </div>
         <div className={style['footer-bottom']}>
