ad731eaddacf6190546dc06286a295e29635b016
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2024-02-28 17:39:41 -0300

    [client] Added http requests to the publication pages

--- client/src/views/Publications.js
+++ client/src/views/Publications.js
@@ -1,14 +1,27 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { NavLink } from 'react-router-dom'
 import { FaStar } from 'react-icons/fa'
+
 import Image from './../components/Image';
-
-
-import freud from './../assets/sigmund-freud-1153858_640.jpg'
 import style from './Publications.module.scss'
 
-const publications = [1,2,3,4,5,6,7,8,9];
-
 export default function Publications () {
+  const [publications, setPublications] = useState([]);
+
+  useEffect(
+    function () {
+      axios.get(process.env.REACT_APP_BACKEND_SERVER + '/publications')
+      .then(
+        function (response) {
+          console.log(response);
+          setPublications(response.data);
+        }
+      )
+    },
+    []
+  );
+
   return (
     <div className='container'>
       <h1>Publicações</h1>
@@ -17,22 +30,22 @@ export default function Publications () {
           function (publication) {
             return (
               <div
-                key={publication}
+                key={publication.id}
                 className={ style['card'] }
               >
-                <NavLink to='/publicacao'>
+                <NavLink to={ '/publicacao?text_code=' + publication.text_code }>
                   <Image
+                    src={publication.image}
+                    alt={publication.image}
                     className={style['image']}
-                    src={freud}
-                    alt='Sigmund Freud'
                     v-align='center'
                     h-align='center'
                   />
                   <div className={style['text']}>
-                    <div className={ style['title'] }>O que é Psicanálise?</div>
-                    <div className={ style['date'] }>10/05/2023</div>
+                    <div className={ style['title'] }>{publication.title}</div>
+                    <div className={ style['date'] }>{publication.date}</div>
                     {
-                      publication === 1 &&
+                      publication.highlight === 1 &&
                       <div className={style['highlight']}>
                         <FaStar /> Destaque
                       </div>
