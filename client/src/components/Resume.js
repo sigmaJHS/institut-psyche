039dc8cafbcf86ebeac21ecd01656e6d27bde6db
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2024-03-01 13:20:37 -0300

    [client] Removed the psychology symbol from the home page
    Psychoanalysis has a different symbol

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -4,7 +4,6 @@ import GoogleMap from './../components/GoogleMap';
 
 import owner from './../assets/marcelo-lopes-psicanalista.jpg'
 import freud from './../assets/sigmund-freud.png'
-import psychologySymbol from './../assets/simbolo-psicologia.png'
 import psyche from './../assets/logotipo-psyche-textless.png'
 
 import style from './Resume.module.scss';
@@ -48,12 +47,6 @@ export default function Resume () {
         />
         <div className='container'>
           <div className={style['about-text']}>
-            <div
-              className={style['psychology-symbol']}
-              style={
-                {backgroundImage: `url('${psychologySymbol}')`}
-              }
-            />
             <h2 className={style['title']}>Sobre a Psicanálise</h2>
             <p className={style['paragraph']}>A Psicanálise, fundada pelo médico neurologista Sigmund Freud, hoje é difundida mundialmente. Entretanto, muitas pessoas ainda confundem o que é Psicanálise e o que não é. Por isso, preparamos este texto para que possa ser entendido melhor o significado desse termo.</p>
             <p className={style['paragraph']}>A psicanálise é uma ciência ou campo do saber relacionada à psicologia, mas não é restrita a psicólogos. Há psicanalistas de diversas áreas do saber. Também não se trata de um tipo de terapia da mesma maneira que a psiquiatria e nem pode ser considerada uma área da medicina. Isso porque o psicanalista não é médico e não pode receitar medicamentos. Apesar disso, há muitos psicólogos e psiquiatras que seguem a psicanálise como área principal ou secundária de atuação, além de profissionais de outras áreas como sociologia, educação, artes etc.</p>
