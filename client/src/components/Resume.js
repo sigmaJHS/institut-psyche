318f5a5a83f315c518dfa776435113bfab695177
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2024-03-05 16:32:18 -0300

    [client] moved the owner's bio to the about page

--- client/src/components/Resume.js
+++ client/src/components/Resume.js
@@ -1,43 +1,13 @@
-import Image from './../components/Image.js'
 import Button from './../components/Button.js'
 import GoogleMap from './../components/GoogleMap';
 
-import owner from './../assets/marcelo-lopes-psicanalista.jpg'
 import freud from './../assets/sigmund-freud.png'
-import psyche from './../assets/logotipo-psyche-textless.png'
 
 import style from './Resume.module.scss';
 
 export default function Resume () {
   return (
     <div className='resume'>
-      <section id={style['owner']}>
-        <div
-          className={style['psyche']}
-          style={
-            {backgroundImage: `url('${psyche}')`}
-          }
-        />
-        <div className='container'>
-          <div className={`column ${style['left']}`}>
-            <h2 className={style['title']}>Prof. Dr. Marcelo Lopes Rodrigues</h2>
-            <p className={style['paragraph']}>Nascido em São Paulo, Capital, em 07 de novembro de 1972, foi criado em Santo André, onde estudou no Instituto Coração de Jesus, formando-se em Patologia Clínica – Nível Técnico em 1990. Em 1991 Forma-se em Enfermagem pelo Colégio ETIP, também em Santo André. Em 1993, mudou-se para Itatiba, interior de São Paulo e vai cursar Farmácia e Bioquímica na Universidade São Francisco em Bragança Paulista.</p>
-            <p className={style['paragraph']}>Trabalhou em diversos hospitais da capital e interior de São Paulo até o ano de 2000, quando entra para o curso de Formação em Psicanálise pelo Instituto Brasileiro de Transpsicanálise em São Paulo, concluindo sua formação em 2004. Realiza desde então palestras em todo o Estado de São Paulo e atende em sua clínica localizada em Itatiba/SP. Dirige o Curso de Formação em Psicanálise no Institut Psyché, por ele fundado em 2006. Concluiu seu mestrado em 2012 no International College of Open Courses. Concluiu seu Doutorado em 2013 na Faculdade Teológica de São Paulo.</p>
-            <Button>Conheça a equipe completa!</Button>
-          </div>
-          <div className={`column ${style['right']}`}>
-            <div
-              className={style['image-holder']}
-            >
-              <Image
-                src={owner}
-                alt='Prof. Dr. Marcelo Lopes Rodrigues'
-                h-align='center'
-              />
-            </div>
-          </div>
-        </div>
-      </section>
       <section id={style['freud']}>
         <div
           className={style['freud']}
