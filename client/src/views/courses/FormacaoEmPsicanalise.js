ab5c865dde475cdeb99d6c7f83e060de916f5309
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2024-03-05 14:45:36 -0300

    Update FormacaoEmPsicanalise.js

--- client/src/views/courses/FormacaoEmPsicanalise.js
+++ client/src/views/courses/FormacaoEmPsicanalise.js
@@ -1,5 +1,103 @@
+import Course from '../Course'
+
 export default function FormacaoEmPsicanalise () {
   return (
-    <div>Formação Em Psicanálise</div>
+    <Course
+      image='/lacan.jpg'
+      title='Curso de Formação em Psicanálise'
+      date='01/10/2023 a 01/10/2026'
+      link_hotmart=''
+      link_symple=''
+    >
+      <p>Olá...</p>
+      <p>O presente Curso de Formação em Psicanálise – modalidade On-Line, foi elaborado pensando nas atuais necessidades, onde a demanda de procura de profissionais a cada dia vem aumentando. O curso está embasado nos preceitos de formação segundo o IPA (Associação Internacional de Psicanálise), sendo o tripé fundamental (teoria, análise didática e supervisão).</p>
+      <p>Nosso curso acontecerá em 15 (quinze) módulos mensais, sendo 4 (quatro) aulas teóricas, as aulas serão transmitidas ao vivo, facilitando assim a interação aluno/professor, sendo cada módulo é pago separadamente. Complementando a teoria, a cada mês o aluno entregará um relatório de leitura das obras de Sigmund Freud, fornecendo assim uma base teórica sólida. A Análise Didática (terapia) que para a Psicanálise é obrigatória, o aluno poderá optar em fazer sua Análise com um de nossos Analistas (via on-line), ou realizar sua análise com Profissional de sua escolha, desde que, a cada 2 meses seja entregue relatório de sua terapia. A Supervisão acontece quando o aluno, após avaliação do responsável pelo Curso de Formação, Prof. Dr. Marcelo L. Rodrigues autorizará os atendimentos sob sua Supervisão.</p>
+      <p>O Aluno terá ao Final do Curso de Formação, desde que tenha cumprido todos os requisitos do curso, o Certificado com a carga horária total, permitindo o mesmo a clinicar, ou seja, a trabalhar como Psicanalista em todo Território Nacional.</p>
+      <h2>O Curso de Formação está dividido em módulos, todos com aulas semanais como descrito a seguir:</h2>
+      <h3>MÓDULO 01</h3>
+      <ul>
+        <li>Aula inaugural – orientações gerais e resolução de dúvidas;</li>
+        <li>Aula 2 – Conceitos Gerais e Definições</li>
+        <li>Aula 3 – Noções Básicas de Psicanálise</li>
+        <li>Aula 4 – Introdução à Psicanálise</li>
+      </ul>
+      <h3>MÓDULO 02</h3>
+      <ul>
+        <li>Aula 1 – Introdução à Psicanálise</li>
+        <li>Aula 2 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 3 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 4 – Introdução à Psicanálise - Continuação</li>
+      </ul>
+      <h3>MÓDULO 03</h3>
+      <ul>
+        <li>Aula 1 – Introdução à Psicanálise</li>
+        <li>Aula 2 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 3 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 4 – Introdução à Psicanálise - Continuação</li>
+      </ul>
+      <h3>MÓDULO 04</h3>
+      <ul>
+        <li>Aula 1 – Introdução à Psicanálise</li>
+        <li>Aula 2 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 3 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 4 – Introdução à Psicanálise - Continuação</li>
+      </ul>
+      <h3>MÓDULO 05</h3>
+      <ul>
+        <li>Aula 1 – Introdução à Psicanálise</li>
+        <li>Aula 2 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 3 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 4 – Introdução à Psicanálise - Continuação</li>
+      </ul>
+      <h3>MÓDULO 06</h3>
+      <ul>
+        <li>Aula 1 – Introdução à Psicanálise</li>
+        <li>Aula 2 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 3 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 4 – Introdução à Psicanálise - Continuação</li>
+      </ul>
+      <h3>MÓDULO 07</h3>
+      <ul>
+        <li>Aula 1 – Introdução à Psicanálise</li>
+        <li>Aula 2 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 3 – Introdução à Psicanálise - Continuação</li>
+        <li>Aula 4 – Introdução à Psicanálise - Continuação</li>
+      </ul>
+      <h3>MÓDULO 08</h3>
+      <ul>
+        <li>Aula 1 – Psicanálise e Psicoterapia</li>
+        <li>Aula 2 – Psicanálise e Psicoterapia- Continuação</li>
+        <li>Aula 3 – Psicanálise e Psicoterapia- Continuação</li>
+        <li>Aula 4 – Introdução à Psicanálise - Continuação</li>
+      </ul>
+      <h3>MÓDULO 09</h3>
+      <ul>
+        <li>Aula 1 – Sociologia das Doenças Mentais</li>
+        <li>Aula 2 – Sociologia das Doenças Mentais - Continuação</li>
+        <li>Aula 3 – Sociologia das Doenças Mentais - Continuação</li>
+        <li>Aula 4 – Revisão</li>
+      </ul>
+      <h3>MÓDULO 10</h3>
+      <ul>
+        <li>Aula 1 – Análise dos Sonhos</li>
+        <li>Aula 2 – Análise dos Sonhos - Continuação</li>
+        <li>Aula 3 – Análise dos Sonhos - Continuação</li>
+        <li>Aula 4 – Revisão</li>
+      </ul>
+      <h3>MÓDULO 11</h3>
+      <ul>
+        <li>Aula 1 – Psicanálise e Complexo de Édipo</li>
+        <li>Aula 2 – Psicanálise e Complexo de Édipo - Continuação</li>
+        <li>Aula 3 – Psicanálise e Complexo de Édipo - Continuação</li>
+        <li>Aula 4 – Revisão</li>
+      </ul>
+      <h3>MÓDULO 12</h3>
+      <ul>
+        <li>Aula 1 – Psicossomática Introdução  </li>
+        <li>Aula 2 – Doenças Psicossomáticas</li>
+        <li>Aula 3 – Medicina Psicossomática</li>
+        <li>Aula 4 – Revisão</li>
+      </ul>
+    </Course>
   );
 }
