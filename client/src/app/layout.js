02be223aa180f8cf888cef7592a5a81c1600ec9e
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2024-03-10 08:19:43 -0300

    [client] Removed the next.js default font family

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -1,7 +1,4 @@
-import { Inter } from "next/font/google";
 import "@/style/main.scss";
-
-const inter = Inter({ subsets: ["latin"] });
 
 import Navbar from '@/components/navbar/Navbar'
 import Footer from '@/components/Footer'
@@ -14,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body>
         <Navbar />
         {children}
         <Footer />
