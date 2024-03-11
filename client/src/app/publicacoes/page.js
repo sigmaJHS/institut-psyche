ede497c91b481e10dfffd21d43e76f836ca828ec
Gabriel Figueiredo Costa <gabrielcostadev@gmail.com>
2024-03-11 10:40:44 -0300

    [client] Hard-coded the single publication page

--- client/src/app/publicacoes/page.js
+++ client/src/app/publicacoes/page.js
@@ -4,25 +4,25 @@ import { FaStar } from 'react-icons/fa'
 import Image from '@/components/Image';
 import style from './page.module.scss'
 
-export default function Publications () {
+export default async function Publications () {
   return (
     <div className='container'>
       <h1>Publicações</h1>
       <div className={ style['card'] }>
-        <Link href={ '/publicacoes/o_que_e_psicanalise' }>
+        <Link href={ '/publicacoes/o-que-e-psicanalise' }>
           <Image
-            src={'sigmund-freud-1153858_640.jpg'}
-            alt={'Sigmund Freud'}
+            src={ 'sigmund-freud-1153858_640.jpg' }
+            alt={ 'O Que é Psicanálise' }
             className={style['image']}
             v-align='center'
             h-align='center'
           />
           <div className={style['text']}>
-            <div className={ style['title'] }>O que é Psicanálise</div>
-            <div className={ style['date'] }>10/05/2023</div>
+            <div className={ style['title'] }>{ 'O Que é Psicanálise' }</div>
+            <div className={ style['date'] }>{ '05/10/2023' }</div>
             <div className={style['highlight']}>
-                <FaStar /> Destaque
-              </div>
+              <FaStar /> Destaque
+            </div>   
           </div>
         </Link>
       </div>
